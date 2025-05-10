50584b4ea309111139e44407b4d749022ba5614a
Valsinev <streamline_@abv.bg>
2025-05-10 21:08:55 +0300

    Finished CRUD methods for Recipe

--- src/main/java/com/belejki/belejki/restful/controller/RecipeController.java
+++ src/main/java/com/belejki/belejki/restful/controller/RecipeController.java
@@ -2,7 +2,6 @@ package com.belejki.belejki.restful.controller;
 
 import com.belejki.belejki.restful.dto.RecipeDto;
 import com.belejki.belejki.restful.entity.Recipe;
-import com.belejki.belejki.restful.entity.User;
 import com.belejki.belejki.restful.exception.RecipeNotFoundException;
 import com.belejki.belejki.restful.mapper.RecipesMapper;
 import com.belejki.belejki.restful.repository.RecipeRepository;
@@ -12,9 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.belejki.belejki.restful.controller.Utility.checkIfOwnerOrAdmin;
+
 
 @RestController
 public class RecipeController {
@@ -33,81 +38,139 @@ public class RecipeController {
 
     //region POST METHODS
 
-    @PostMapping("/user/recipes/{userId}")
-    public ResponseEntity<RecipeDto> save(@Valid @RequestBody RecipeDto recipe, @PathVariable Long userId) {
-        Recipe save = recipeService.save(recipe, userId);
+    @PostMapping("/user/recipes/id/{userId}")
+    public ResponseEntity<RecipeDto> saveByUserId(@Valid @RequestBody RecipeDto recipe, @PathVariable Long userId) {
+        Recipe save = recipeService.saveByUserId(recipe, userId);
         return ResponseEntity.ok(recipesMapper.toDto(save, userId));
+    }
+
+    @PostMapping("/user/recipes/{username}")
+    public ResponseEntity<RecipeDto> saveByUsername(@Valid @RequestBody RecipeDto recipe, @PathVariable String username) {
+        Recipe save = recipeService.saveByUsername(recipe, username);
+        return ResponseEntity.ok(recipesMapper.toDto(save, save.getUser().getId()));
     }
 
     //endregion
 
     //region GET METHODS
 
-    @GetMapping
-    public Page<Recipe> findAll(Pageable pageable) {
-        return recipeRepository.findAll(pageable);
+    @GetMapping("/admin/recipes")
+    public Page<RecipeDto> findAll(Pageable pageable) {
+        Page<Recipe> all = recipeRepository.findAll(pageable);
+        return all.map(recipe -> recipesMapper.toDto(recipe, recipe.getUser().getId()));
     }
 
-    @GetMapping("/id/{id}")
-    public Recipe findById(@PathVariable Long id) {
-        return recipeRepository.findById(id)
+    @GetMapping("/user/recipes/id/{id}")
+    //user can only find his own recipes by id
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER'")
+    public RecipeDto findById(@PathVariable Long id, Authentication authentication) {
+        Recipe recipe = recipeRepository.findById(id)
                 .orElseThrow(
                         () -> new RecipeNotFoundException("No recipe found with id = " + id)
                 );
+        String username = recipe.getUser().getUsername();
+        boolean access = checkIfOwnerOrAdmin(authentication, username);
+        if (!access) {
+            throw new AccessDeniedException("Only owner or admin can access this recipe.");
+        }
+        return recipesMapper.toDto(recipe, recipe.getUser().getId());
     }
 
-    @GetMapping("/{name}")
-    public Recipe findByName(@PathVariable String name) {
-        return recipeRepository.findByName(name);
+    @GetMapping("/user/recipes")
+    public Page<RecipeDto> findById(Authentication authentication, Pageable pageable) {
+        String username = authentication.getName();
+        Page<Recipe> allByUserUsername = recipeService.findAllByUser_Username(username, pageable);
+        Page<RecipeDto> dto = allByUserUsername.map(recipe ->
+                recipesMapper.toDto(recipe, recipe.getUser().getId()));
+        return dto;
     }
 
-    @GetMapping("/user/recipes/find/{name}")
-    public Page<Recipe> findAllByNameContaining(@PathVariable String name, @RequestBody User user, Pageable pageable) {
-        return recipeRepository.findAllByNameContainingAndUser(name, user, pageable);
+    @GetMapping("/admin/recipes/{recipeName}")
+    public List<RecipeDto> findAllByNameContainingIgnoreCase(@PathVariable String recipeName, Pageable pageable) {
+        Page<Recipe> byNameContainingIgnoreCase = recipeRepository.findAllByNameContainingIgnoreCase(recipeName, pageable);
+        List<RecipeDto> foundedDto = byNameContainingIgnoreCase.stream()
+                .map(recipe ->
+                        recipesMapper.toDto(recipe, recipe.getUser().getId()))
+                .toList();
+        return foundedDto;
     }
 
-    @GetMapping("{username}/by-ingredients")
-    public Page<Recipe> findAllByIngredients(@RequestParam List<String> ingredients,
-                                             @PathVariable String username,
-                                             Pageable pageable) {
-        return recipeService.findRecipesByAllIngredientNamesAndUsername(ingredients, username, pageable);
+    @GetMapping("/admin/recipes/user/id/{id}")
+    public List<RecipeDto> findAllByUserId(@PathVariable Long id, Pageable pageable) {
+        Page<Recipe> allByUserId = recipeService.findAllByUser_Id(id, pageable);
+        List<RecipeDto> foundedDto = allByUserId.stream()
+                .map(recipe ->
+                        recipesMapper.toDto(recipe, recipe.getUser().getId()))
+                .toList();
+        return foundedDto;
     }
 
-    @GetMapping("/by-ingredients")
-    public Page<Recipe> findAllByIngredients(@RequestParam List<String> ingredients,
-                                             @RequestBody User user,
-                                             Pageable pageable) {
-        return recipeService.findRecipesByAllIngredientNamesAndUser(ingredients, user, pageable);
+    @GetMapping("/admin/recipes/user/{username}")
+    public List<RecipeDto> findAllByUserId(@PathVariable String username, Pageable pageable) {
+        Page<Recipe> allByUserId = recipeService.findAllByUser_Username(username, pageable);
+        List<RecipeDto> foundedDto = allByUserId.stream()
+                .map(recipe ->
+                        recipesMapper.toDto(recipe, recipe.getUser().getId()))
+                .toList();
+        return foundedDto;
+    }
+
+    @GetMapping("/user/recipes/{recipeName}")
+    public List<RecipeDto> findAllOwnedByNameContainingIgnoreCase(@PathVariable String recipeName, Authentication authentication, Pageable pageable) {
+        String username = authentication.getName();
+        Page<Recipe> byNameContainingIgnoreCase = recipeRepository.findAllByNameContainingAndUser_Username(recipeName, username, pageable);
+        List<RecipeDto> foundedDto = byNameContainingIgnoreCase.stream()
+                .map(recipe ->
+                recipesMapper.toDto(recipe, recipe.getUser().getId()))
+                .toList();
+        return foundedDto;
+    }
+
+
+    @GetMapping("/user/recipes/by-ingredients")
+    public Page<RecipeDto> findAllByIngredients(@RequestParam List<String> ingredients,
+                                             Pageable pageable,
+                                             Authentication authentication) {
+        String username = authentication.getName();
+        Page<Recipe> recipesByAllIngredientNamesAndUserUsername = recipeService.findRecipesByAllIngredientNamesAndUser_Username(ingredients, username, pageable);
+        return recipesByAllIngredientNamesAndUserUsername.map(recipe -> recipesMapper.toDto(recipe, recipe.getUser().getId()));
     }
 
     //endregion
 
     //region DELETE METHODS
 
-    @DeleteMapping
-    public ResponseEntity delete(@RequestBody Recipe recipe) {
-        recipeRepository.delete(recipe);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/user/recipes")
+    public ResponseEntity<RecipeDto> delete(@RequestBody Recipe recipe) {
+        Recipe deleted = recipeService.delete(recipe);
+        return ResponseEntity.ok(recipesMapper.toDto(deleted, deleted.getUser().getId()));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity deleteById(@PathVariable Long id) {
-        recipeRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/user/recipes/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER'")
+    public ResponseEntity<RecipeDto> deleteById(@PathVariable Long id, Authentication authentication) {
+        Recipe recipe = recipeService.findById(id);
+        boolean access = checkIfOwnerOrAdmin(authentication, recipe.getUser().getUsername());
+        if (!access) {
+            throw new AccessDeniedException("Only owner or admin can delete this recipe.");
+        }
+        Recipe deleted = recipeService.delete(recipe);
+        return ResponseEntity.ok(recipesMapper.toDto(deleted, deleted.getUser().getId()));
     }
 
-    @DeleteMapping("/{username}")
-    public ResponseEntity deleteAllByUser_Username(@PathVariable String username) {
-        List<Recipe> byUserUsername = recipeRepository.findByUser_Username(username);
-        recipeRepository.deleteAll(byUserUsername);
-        return ResponseEntity.noContent().build();
+    @DeleteMapping("/admin/recipes/user/id/{id}")
+    public ResponseEntity<Page<RecipeDto>> deleteAllByUser_Username(@PathVariable Long id, Pageable pageable) {
+        Page<Recipe> allByUserId = recipeService.findAllByUser_Id(id, pageable);
+        recipeRepository.deleteAll(allByUserId);
+        return ResponseEntity.ok(allByUserId.map(recipe -> recipesMapper.toDto(recipe, recipe.getUser().getId())));
     }
 
-    @DeleteMapping("/by-user")
-    public ResponseEntity deleteByUser_Username(@RequestBody User user) {
-        List<Recipe> byUser = recipeRepository.findByUser(user);
-        recipeRepository.deleteAll(byUser);
-        return ResponseEntity.noContent().build();
+
+    @DeleteMapping("/admin/recipes/user/{username}")
+    public ResponseEntity<Page<RecipeDto>> deleteAllByUser_Username(@PathVariable String username, Pageable pageable) {
+        Page<Recipe> allByUser_username = recipeService.findAllByUser_Username(username, pageable);
+        recipeRepository.deleteAll(allByUser_username);
+        return ResponseEntity.ok(allByUser_username.map(recipe -> recipesMapper.toDto(recipe, recipe.getUser().getId())));
     }
 
     //endregion
--- src/main/java/com/belejki/belejki/restful/service/RecipeService.java
+++ src/main/java/com/belejki/belejki/restful/service/RecipeService.java
@@ -23,31 +23,24 @@ import java.util.List;
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
-    private final UserRepository userRepository;
     private final UserService userService;
     private final RecipesMapper recipesMapper;
     private final RecipeIngredientMapper recipeIngredientMapper;
     private final IngredientService ingredientService;
 
     @Autowired
-    public RecipeService(RecipeRepository recipeRepository, UserRepository userRepository, UserService userService, RecipesMapper recipesMapper, RecipeIngredientMapper recipeIngredientMapper, IngredientService ingredientService) {
+    public RecipeService(RecipeRepository recipeRepository, UserService userService, RecipesMapper recipesMapper, RecipeIngredientMapper recipeIngredientMapper, IngredientService ingredientService) {
         this.recipeRepository = recipeRepository;
-        this.userRepository = userRepository;
         this.userService = userService;
         this.recipesMapper = recipesMapper;
         this.recipeIngredientMapper = recipeIngredientMapper;
         this.ingredientService = ingredientService;
     }
 
-    public Page<Recipe> findRecipesByAllIngredientNamesAndUser(List<String> ingredients, User user, Pageable pageable) {
-        return recipeRepository.findRecipesByAllIngredientNamesAndUser(ingredients, ingredients.size(), user, pageable);
-    }
 
-    public Page<Recipe> findRecipesByAllIngredientNamesAndUsername(List<String> ingredients, String username, Pageable pageable) {
-        return recipeRepository.findRecipesByAllIngredientNamesAndUsername(ingredients, ingredients.size(), username, pageable);
-    }
+    //Post
 
-    public Recipe save(RecipeDto dto, Long userId) {
+    public Recipe saveByUserId(RecipeDto dto, Long userId) {
 
         User user = userService.findById(userId);
         Recipe recipe = new Recipe();
@@ -64,7 +57,47 @@ public class RecipeService {
         return finalRecipie;
     }
 
+    public Recipe saveByUsername(RecipeDto dto, String username) {
+        User user = userService.findByUsername(username);
+        Recipe recipe = new Recipe();
+        List<RecipeIngredientDto> recipeIngredientsDto = dto.getIngredients();
+        List<RecipeIngredient> recipeIngredients = recipeIngredientsDto
+                .stream()
+                .map(recipeIngredientDto ->
+                        recipeIngredientMapper.toEntity(recipeIngredientDto,
+                                recipe,
+                                ingredientService.findOrCreateByName(recipeIngredientDto.getIngredient())))
+                .toList();
+        Recipe finalRecipie = recipesMapper.toEntity(dto, user, recipeIngredients);
+        recipeRepository.save(finalRecipie);
+        return finalRecipie;
+    }
+
+    //Get
+
+    public Page<Recipe> findAllByUser_Id(Long id, Pageable pageable) {
+        return recipeRepository.findAllByUser_Id(id, pageable);
+    }
+
+    public Page<Recipe> findAllByUser_Username(String username, Pageable pageable) {
+        return recipeRepository.findAllByUser_Username(username, pageable);
+    }
+
+    public Page<Recipe> findRecipesByAllIngredientNamesAndUser_Username(List<String> ingredients, String username, Pageable pageable) {
+        return recipeRepository.findRecipesByAllIngredientNamesAndUsername(ingredients, ingredients.size(), username, pageable);
+    }
+
     public Recipe findById(@NonNull Long recipeId) {
         return recipeRepository.findById(recipeId).orElseThrow(() -> new RecipeNotFoundException("Recipe not found for id: " + recipeId));
     }
+
+    //Delete
+
+    public Recipe delete(Recipe recipe) {
+        Recipe deleted = findById(recipe.getId());
+        recipeRepository.delete(deleted);
+        return deleted;
+    }
+
+
 }
